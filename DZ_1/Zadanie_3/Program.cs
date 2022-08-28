d3d696916c382ae00a5d0e0d54adbf8ad47a7a3f
EvgeniaMyasnikova <109282358+EvgeniaMyasnikova@users.noreply.github.com>
2022-08-29 01:39:24 +0500

    Update Program.cs

--- DZ_1/Zadanie_3/Program.cs
+++ DZ_1/Zadanie_3/Program.cs
@@ -15,9 +15,9 @@ Console.Write("Введите количество столбцов: ");
 int columns = Convert.ToInt32(Console.ReadLine());
 
 int[,] array =  new int[rows, columns];
-for (int i = 0; i < rows; i++)
+for (int i = 0; i < array.GetLength(0); i++)
 {
-    for (int j = 0; j < columns; j++)
+    for (int j = 0; j < array.GetLength(1); j++)
     {
         array[i, j] = new Random().Next(100);
         Console.Write(array[i, j] + "\t");
